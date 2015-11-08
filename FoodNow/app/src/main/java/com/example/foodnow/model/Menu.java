fcd343c234418f3a9fa9097ceb47e149047abe97
BlueShoelaces <narruda@emich.edu>
2015-11-07 20:48:03 -0500

    my half of the object model shell classes

--- FoodNow/app/src/main/java/com/example/foodnow/model/Menu.java
+++ FoodNow/app/src/main/java/com/example/foodnow/model/Menu.java
@@ -1,9 +1,22 @@
 package com.example.foodnow.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Menu {
-    private ArrayList<Item> items;
 
+    private String id;
+    private String name;
+    private ArrayList<MenuChild> children;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<MenuChild> getChildren() {
+        return children;
+    }
 }
