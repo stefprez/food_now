8176e9fd87dfec2a525acb8c1fedfeaf9b2d9ac1
Stefano Prezioso <stefprez@gmail.com>
2015-11-07 21:07:39 -0500

    Updated Model objects

--- FoodNow/app/src/main/java/com/example/foodnow/model/OptionGroup.java
+++ FoodNow/app/src/main/java/com/example/foodnow/model/OptionGroup.java
@@ -3,18 +3,43 @@ package com.example.foodnow.model;
 import java.util.ArrayList;
 
 public class OptionGroup implements MenuChild {
+    private String id;
+    private String name;
+    private String description;
+    private int minSelection;
+    private int maxSelection;
+    private boolean selDep;
+    private ArrayList<MenuChild> children;
+
     @Override
     public String getId() {
-        return null;
+        return id;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
     }
 
     @Override
     public ArrayList<MenuChild> getChildren() {
-        return null;
+        return children;
+    }
+
+    public int getMaxSelection() {
+        return maxSelection;
+    }
+
+    public int getMinSelection() {
+        return minSelection;
+    }
+
+    public boolean isSelDep() {
+        return selDep;
     }
 }
