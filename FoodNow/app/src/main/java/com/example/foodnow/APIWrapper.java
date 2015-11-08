27b6bfa5587991ca9ae080d932415b27a98e1075
rwitting <wittinro@gmail.com>
2015-11-07 21:56:19 -0500

    Move APIWrapper class

--- FoodNow/app/src/main/java/com/example/foodnow/APIWrapper.java
+++ FoodNow/app/src/main/java/com/example/foodnow/APIWrapper.java
@@ -1,4 +1,4 @@
-package example.foodnow;
+package com.example.foodnow;
 
 import android.os.AsyncTask;
 import android.util.Log;
--- FoodNow/app/src/main/java/com/example/foodnow/MainActivity.java
+++ FoodNow/app/src/main/java/com/example/foodnow/MainActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import example.foodnow.APIWrapper;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
