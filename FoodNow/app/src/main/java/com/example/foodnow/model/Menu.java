722d3dad926ec5599049b71194907ed280c5de1a
Stefano Prezioso <stefprez@gmail.com>
2015-11-07 22:44:17 -0500

    Added Annotations for JSON to Model Objects

--- FoodNow/app/src/main/java/com/example/foodnow/model/Menu.java
+++ FoodNow/app/src/main/java/com/example/foodnow/model/Menu.java
@@ -1,12 +1,21 @@
 package com.example.foodnow.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 
 public class Menu {
-
+    @SerializedName("id")
     private String id;
+
+    @SerializedName("name")
     private String name;
+
+    @SerializedName("children")
     private ArrayList<MenuChild> children;
+
+    @SerializedName("description")
+    private String description;
 
     public String getId() {
         return id;
@@ -18,5 +27,9 @@ public class Menu {
 
     public ArrayList<MenuChild> getChildren() {
         return children;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
